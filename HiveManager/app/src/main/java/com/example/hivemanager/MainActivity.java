c751e194d7a2a5191ee9a87596a45df65aad3157
Jettypile <36287441+Jettypile@users.noreply.github.com>
2020-09-30 14:53:39 -0500

    updated main with working upload pls check

--- HiveManager/app/src/main/java/com/example/hivemanager/MainActivity.java
+++ HiveManager/app/src/main/java/com/example/hivemanager/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.hivemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button Login_Button, CreateNewAccount_Button, Upload_Button;
@@ -17,26 +32,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
              NewAccountAddress_PlainText, NewAccountPhone_PlainText, NewAccountEmail_PlainText;
     //ImageView uploadPicture_ImageView;
     private static final int PICK_IMAGE = 100;
+    public static final int GET_FROM_GALLERY = 3;
     Uri imageUri;
     ImageView imageView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        imageView = (ImageView)findViewById(R.id.IDProf);
         Login_Button = (Button) findViewById(R.id.Login_Button);
         CreateNewAccount_Button = (Button) findViewById(R.id.CreateNewAccount_Button);
-        Upload_Button = (Button) findViewById(R.id.Upload_Button);
 
         Upload_Button = (Button) findViewById(R.id.Upload_Button);
-
         Upload_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openGallery();
             }
         });
+
+
 
         LogicUsername_PlainText = (EditText) findViewById(R.id.LogicUsername_PlainText);
         LoginPassword_PlainText = (EditText) findViewById(R.id.LoginPassword_PlainText);
@@ -57,16 +72,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
         startActivityForResult(gallery, PICK_IMAGE);
     }
-
     @Override
-    protected void onActivityResult(int requestCode,int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
-        if(requestCode == RESULT_OK && requestCode == PICK_IMAGE){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
             imageUri = data.getData();
             imageView.setImageURI(imageUri);
         }
-
     }
+
+
+
 
     @Override
     public void onClick(View v) {
