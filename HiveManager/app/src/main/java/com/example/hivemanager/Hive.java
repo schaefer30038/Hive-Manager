f8c6916302727bc194f4c3ca708f18e14cb754a9
andy-gomez <56177034+andy-gomez@users.noreply.github.com>
2020-09-30 21:28:29 -0500

    Add files via upload

---
+++ HiveManager/app/src/main/java/com/example/hivemanager/Hive.java
@@ -0,0 +1,77 @@
+package com.example.hivemanager;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class Hive extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hive);
+
+        listView = (ListView)findViewById(R.id.listview);
+
+        final ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+        arrayList.add("testing");
+        arrayList.add("this");
+        arrayList.add("thing");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Hive.this, "clicked item: "+position+" "+arrayList.get(position).toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
