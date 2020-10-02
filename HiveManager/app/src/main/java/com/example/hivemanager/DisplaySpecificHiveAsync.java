e391ee10ace17575e251f098659328f78ec4304d
andy-gomez <56177034+andy-gomez@users.noreply.github.com>
2020-10-02 05:08:03 -0500

    Update DisplaySpecificHiveAsync.java

--- HiveManager/app/src/main/java/com/example/hivemanager/DisplaySpecificHiveAsync.java
+++ HiveManager/app/src/main/java/com/example/hivemanager/DisplaySpecificHiveAsync.java
@@ -2,15 +2,18 @@ package com.example.hivemanager;
 
 import android.os.AsyncTask;
 
+import java.sql.ResultSet;
+
 public class DisplaySpecificHiveAsync extends AsyncTask<String, Void, Void> {
     static SQLConnection connect;
+    static ResultSet resultSet;
     @Override
     protected Void doInBackground(String... strings) {
         connect = new SQLConnection();
         String username = strings[0];
         String apiary = strings[1];
         String hive = strings[2];
-        connect.displaySpecificHive(username, apiary, hive);
+        resultSet = connect.displaySpecificHive(username, apiary, hive);
         MainActivity.done = true;
         return null;
     }
