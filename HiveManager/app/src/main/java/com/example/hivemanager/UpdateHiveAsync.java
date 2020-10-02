c0991abe2767fd67414f57e12fda920c85fe8ae8
dhondup2 <32646210+dhondup2@users.noreply.github.com>
2020-10-01 22:50:57 -0500

    Update UpdateHiveAsync.java

--- HiveManager/app/src/main/java/com/example/hivemanager/UpdateHiveAsync.java
+++ HiveManager/app/src/main/java/com/example/hivemanager/UpdateHiveAsync.java
@@ -9,16 +9,17 @@ public class UpdateHiveAsync extends AsyncTask<String,Void,Void> {
         SQLConnection connect = new SQLConnection();
         String username = strings[0];
         String apiary = strings[1];
-        String hive = strings[2];
-        String inspection = strings[3];
-        String health = strings[4];
-        String honey = strings[5];
-        String queenproduction = strings[6];
-        String equiphive = strings[7];
-        String equipinven = strings[8];
-        int loss = Integer.parseInt(strings[9]);
-        int gain = Integer.parseInt(strings[10]);
-        connect.updateHive(username, apiary, hive, inspection, health, honey, queenproduction, equiphive, equipinven,
+        String oldhive = strings[2];
+        String newhive = strings[3];
+        String inspection = strings[4];
+        String health = strings[5];
+        String honey = strings[6];
+        String queenproduction = strings[7];
+        String equiphive = strings[8];
+        String equipinven = strings[9];
+        int loss = Integer.parseInt(strings[10]);
+        int gain = Integer.parseInt(strings[11]);
+        connect.updateHive(username, apiary, oldhive, newhive, inspection, health, honey, queenproduction, equiphive, equipinven,
                 loss, gain);
         connect.closeConnection();
         return null;
